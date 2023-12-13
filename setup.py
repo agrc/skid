3e3c444c21b50014755e1559b2b2bcab61b0275f
Jake Adams <jdadams@utah.gov>
2023-12-13 10:09:43 -0700

    chore: update, sync with python template

--- setup.py
+++ setup.py
@@ -4,62 +4,62 @@
 setup.py
 A module that installs the SKIDNAME skid as a module
 """
-from glob import glob
-from os.path import basename, splitext
+from pathlib import Path
 
 from setuptools import find_packages, setup
 
 #: Load version from source file
 version = {}
-with open('src/skidname/version.py') as fp:
-    exec(fp.read(), version)
+version_file = Path(__file__).parent / "src" / "skidname" / "version.py"
+exec(version_file.read_text(), version)
 
 setup(
-    name='skidname',
-    version=version['__version__'],
-    license='MIT',
-    description='Update the PROJECTNAME data from SOURCE via GCF',
-    author='UGRC',
-    author_email='ugrc@utah.gov',
-    url='https://github.com/agrc/skid',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    name="skidname",
+    version=version["__version__"],
+    license="MIT",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    author="UGRC",
+    author_email="ugrc-developers@utah.gov",
+    url="https://github.com/agrc/skid",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=True,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Issue Tracker': 'https://github.com/agrc/python/issues',
+        "Issue Tracker": "https://github.com/agrc/skid/issues",
     },
-    keywords=['gis'],
+    keywords=["gis"],
     install_requires=[
-        'arcgis>=2.0,<2.3',
-        'ugrc-palletjack>=2.2,<4.3',
-        'agrc-supervisor==3.0.*',
+        "ugrc-palletjack>=2.2,<4.4",
+        "agrc-supervisor==3.0.*",
     ],
     extras_require={
-        'tests': [
-            'pylint-quotes~=0.2',
-            'pylint>=2.11,<4.0',
-            'pytest-cov>=3,<5',
-            'pytest-instafail~=0.4',
-            'pytest-isort>=2,<4',
-            'pytest-pylint~=0.18',
-            'pytest-watch~=4.2',
-            'pytest>=6,<8',
-            'yapf~=0.31',
-            'functions-framework',
+        "tests": [
+            "pytest-cov>=3,<5",
+            "pytest-instafail==0.5.*",
+            "pytest-mock==3.*",
+            "pytest-ruff==0.*",
+            "pytest-watch~=4.*",
+            "pytest>=6,<8",
+            "black>=23.3,<23.10",
+            "ruff==0.0.*",
+            "functions-framework",
         ]
     },
     setup_requires=[
-        'pytest-runner',
+        "pytest-runner",
     ],
-    entry_points={'console_scripts': [
-        'skidname = skidname.main:main',
-    ]},
+    entry_points={
+        "console_scripts": [
+            "skidname = skidname.main:function",
+        ]
+    },
 )
